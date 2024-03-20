bfbd3515e17bcbbb96865e1195ca63eef373c50d
Zac Rosenbauer <zac@joggr.io>
2024-03-20 13:07:12 -0400

    Update docker-build-debug-output.sh

--- docker/scripts/docker-build-debug-output.sh
+++ docker/scripts/docker-build-debug-output.sh
@@ -9,11 +9,11 @@
 #   - Increases the log output so you can get more data (i.e. yarn.lock)
 #   - Uses plain output and no-cache so you always get a fresh hit on commands (i.e. RUN ls -la)
 # @example
-#   Copy the file to your directory: `cp ./scripts/docker/docker-debug-output.sh ./$MY_DIR/docker-debug-output.sh`
-#   and add as executable `chmod u+x ./$MY_DIR/docker-debug-output.sh` and run!
+#   Copy the file to your directory: `cp ./scripts/docker/docker-build-debug-output.sh ./$MY_DIR/docker-build-debug-output.sh`
+#   and add as executable `chmod u+x ./$MY_DIR/docker-build-debug-output.sh` and run!
 #
-#   `cd ./$MY_DIR && ./docker-debug-output.sh`
+#   `cd ./$MY_DIR && ./docker-build-debug-output.sh`
 # 
 
 docker buildx create --use --name larger_log --driver-opt env.BUILDKIT_STEP_LOG_MAX_SIZE=50000000
-docker buildx build --progress plain --no-cache . -t debug 2>&1 | tee docker-debug-output.log
+docker buildx build --progress plain --no-cache . -t debug 2>&1 | tee docker-build-debug-output.log
