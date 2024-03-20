c2c77857f6c9f855e3179e47ff5bad6e3115929a
Zac Rosenbauer <zac@joggr.io>
2024-03-20 13:05:48 -0400

    Update docker-debug-output.sh

--- scripts/docker/docker-debug-output.sh
+++ scripts/docker/docker-debug-output.sh
@@ -5,10 +5,15 @@
 #==========================================================================
 #
 # @decription Use this to log output to a file so you can debug your Dockerfile
-# @features:
-#   - Ups the output so you can get more data (i.e. yarn.lock)
+#  ### features
+#   - Increases the log output so you can get more data (i.e. yarn.lock)
 #   - Uses plain output and no-cache so you always get a fresh hit on commands (i.e. RUN ls -la)
+# @example
+#   Copy the file to your directory: `cp ./scripts/docker/docker-debug-output.sh ./$MY_DIR/docker-debug-output.sh`
+#   and add as executable `chmod u+x ./$MY_DIR/docker-debug-output.sh` and run!
+#
+#   `cd ./$MY_DIR && ./docker-debug-output.sh`
 # 
 
 docker buildx create --use --name larger_log --driver-opt env.BUILDKIT_STEP_LOG_MAX_SIZE=50000000
-docker buildx --progress=plain --no-cache . -t test-12 2>&1 | tee docker-output.log
+docker buildx build --progress plain --no-cache . -t debug 2>&1 | tee docker-debug-output.log
